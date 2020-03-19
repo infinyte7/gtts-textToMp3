a46217fb0afc56365b8f9d66de958da57165ad90
Mani <12841290+infinyte7@users.noreply.github.com>
2020-03-19 23:45:23 +0530

    Create tts.py

---
+++ tts.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+
+def textToMp3(text):
+    tts = gTTS(text, lang="zh-cn")
+    
+    tts.save("cmn-"+text+".mp3")
+
+with open("in_HSK2012_all_missing-audios.o.txt", encoding="utf8") as f:
+    for line in f:
+        for word in line.split():
+            try:
+                textToMp3(word)
+                print(word)
+            except Exception:
+                pass
